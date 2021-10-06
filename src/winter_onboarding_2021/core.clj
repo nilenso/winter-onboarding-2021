186491ca7d7c98931f48197a444d1691f15daaef
Alisha Mohanty <alisha@nilenso.com>
2021-10-06 13:51:21 +0530

    Add the tasks done on 6th Oct

--- src/winter_onboarding_2021/core.clj
+++ src/winter_onboarding_2021/core.clj
@@ -12,3 +12,15 @@
 (print (str 1 2 3))
 
 (print (+ 1 3) (+ 1 2 3) 4)
+
+(defn selfie
+  "Given a function `f`, return the result of
+  applying `f` to itself."
+  [f]
+  (f f))
+
+(= 42
+   (identity 42)
+   ((selfie identity) 42)
+   ((selfie (selfie identity)) 42)
+   ((selfie (selfie (selfie identity))) 42)) ; ad-infinitum
