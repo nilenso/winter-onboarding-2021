f901ccd2f53909138421fa3a1eb0b5c19142e7c6
Alisha Mohanty <alisha@nilenso.com>
2021-10-07 23:18:59 +0530

    Add exercise for conj function on sets and maps

--- src/winter_onboarding_2021/4clojure/alisha.clj
+++ src/winter_onboarding_2021/4clojure/alisha.clj
@@ -9,3 +9,11 @@
 (defn problem7 [] (conj [1 2] 3 4))
 
 (defn problem8 [] (set '(:a :a :b :c :c :c :c :d :d)))
+
+(defn problem9 [] (conj #{1 4 3} 2))
+
+(defn problem10_1 [] ((hash-map :a 10, :b 20, :c 30) :b))
+
+(defn problem10_2 [] (:b {:a 10, :b 20, :c 30}))
+
+
