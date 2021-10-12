367a935e82c61bb25a2acb229e4830212e361e60
Yogiraj H <admin@dexteroot.ml>
2021-10-12 15:31:38 +0530

    Solutions till problem 22

--- src/winter_onboarding_2021/4clojure/yogi.clj
+++ src/winter_onboarding_2021/4clojure/yogi.clj
@@ -14,3 +14,29 @@
 (defn prob8 [] #{:a :b :c :d})
 
 (defn prob9 [] (conj #{1 4 3} 2))
+
+(defn prob10 [] 20)
+
+(defn prob11 [] (conj {:a 1} {:b 2} [:c 3]))
+
+(defn prob12 [] 3)
+
+(defn prob13 [] [20 30 40])
+
+(defn prob14actual [] 8)
+
+(defn prob15 [x] (* 2 x))
+
+(defn prob16 [x] (clojure.string/join (conj ["Hello, " x, "!"])))
+
+(defn prob17 [] '(6 7 8))
+
+(defn prob18 [] '(6 7))
+
+(defn prob19 [] 5)
+
+(defn prob20 [] 4)
+
+(defn prob21 [] [5 6])
+
+(defn prob22 [] 4)
