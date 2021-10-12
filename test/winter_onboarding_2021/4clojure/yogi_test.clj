78568ad34e579db930799aee384ab473b6038dd7
Yogiraj H <admin@dexteroot.ml>
2021-10-12 15:31:38 +0530

    Solutions till problem 22

--- test/winter_onboarding_2021/4clojure/yogi_test.clj
+++ test/winter_onboarding_2021/4clojure/yogi_test.clj
@@ -32,3 +32,57 @@
 (deftest check_set_conj
   (testing "set conj")
   (is (= (solutions/prob9) #{1 2 3 4})))
+
+(deftest check_map
+  (testing "map")
+  (is (= (solutions/prob10) ((hash-map :a 10, :b 20, :c 30) :b))))
+
+(deftest check_conj_map
+  (testing "conj on map")
+  (is (= (solutions/prob11) {:a 1, :b 2, :c 3})))
+
+
+(deftest check_map_index
+  (testing "conj on map")
+  (is (= (solutions/prob12)
+         (second [2 3 4]))))
+
+(deftest check_rest_on_map
+  (testing "rest on map")
+  (is (= (solutions/prob13) (rest [10 20 30 40]))))
+
+(deftest prob14actual
+  (testing "functions")
+  (is (= (solutions/prob14actual) ((partial + 5) 3))))
+
+(deftest prob15
+  (testing "double function")
+  (is (= (solutions/prob15 6) 12)))
+
+(deftest prob16
+  (testing "String append")
+  (is (= (solutions/prob16 "Rhea") "Hello, Rhea!")))
+
+(deftest prob17
+  (testing "map function")
+  (is (= (solutions/prob17) (map #(+ % 5) '(1 2 3)))))
+
+(deftest prob18
+  (testing "filters")
+  (is (= (solutions/prob18) (filter #(> % 5) '(3 4 5 6 7)))))
+
+(deftest prob19
+  (testing "last without using 'last'")
+  (is (= (solutions/prob19) (first (reverse [1 2 3 4 5])))))
+
+(deftest prob20
+  (testing "second last")
+  (is (= (solutions/prob20) (second (reverse [1 2 3 4 5])))))
+
+(deftest prob21
+  (testing "nth")
+  (is (= (solutions/prob21) ((fn [x n] ((vec x) n)) '([1 2] [3 4] [5 6]) 2))))
+
+(deftest prob22
+  (testing"count")
+  (is (= (solutions/prob22) (reduce (fn [x y] (inc x)) 0 [12 3 4 5]))))
