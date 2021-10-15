849eea070397b7f0e72cfe8e08892481bc19953a
Shivam Singhal <shivam@nilenso.com>
2021-10-15 17:59:31 +0530

    add solutions to problem 43 and 75

--- src/winter_onboarding_2021/4clojure/shivam.clj
+++ src/winter_onboarding_2021/4clojure/shivam.clj
@@ -1,4 +1,6 @@
-(ns winter-onboarding-2021.4clojure.shivam)
+(ns winter-onboarding-2021.4clojure.shivam
+  (:require [clojure.set :as set]
+            [clojure.string :as str]))
 
 ;; define a function my-map which should take a function and collection 
 
@@ -164,17 +166,107 @@
                          (= (apply str (reverse coll)) coll)
                          (= (reverse coll) coll)))
 
+
 (defn problem28 [coll] (reduce
                         (fn [acc, ele] (concat acc (if (coll? ele)
                                                      (problem28 ele)
                                                      [ele]))) [] coll))
 
-(defn problem29 [test-str] (apply str (filter
-                                       (fn [c] (Character/isUpperCase c))
-                                       test-str)))
+(defn problem29 [test-str]
+  (apply str
+         (filter
+          (fn [c] (Character/isUpperCase c))
+          test-str)))
 
 (defn problem30 [to-compress-seq] (if (< (count to-compress-seq) 2)
                                     to-compress-seq
                                     (if (= (first to-compress-seq) (second to-compress-seq))
                                       (problem30 (rest to-compress-seq))
                                       (conj (problem30 (rest to-compress-seq)) (first to-compress-seq)))))
+
+(defn problem32 [to-duplicate-seq] (reduce (fn [acc, ele] (conj acc ele ele)) (empty to-duplicate-seq) to-duplicate-seq))
+
+
+
+
+;; `n` is the number of seqs in the resultant seq
+;; `iterations` is the number of elements in each seq
+;; `accum` is the resultant seq
+(defn problem43 [my-seq n] (let [iterations (/ (count my-seq) n) accum []]
+                             (conj (for [i (range iterations)]
+                                     (for [j (range n)]
+                                       ((vec my-seq) ((* i iterations) + j)))))))
+
+
+
+;; instead of building all child seqs together,
+;; build one child seq at one time and then conj that child seq to parent seq `accum`
+;; my-seq: the flattened seq
+
+(defn problem43
+  [flattened-seq num-child]
+  ;; `n`is the number of child seqs & `iterations` is the number of elemnents in each child seq
+  (let [desearlize (fn [acc, [index values]] (assoc acc index (map second values)))]
+    (reduce desearlize [] (group-by first (for [i (range (count flattened-seq))]
+                                            (let [seq-index (rem i num-child)]
+                                              [seq-index (nth flattened-seq i)]))))))
+
+
+
+
+
+;; (defn differs-by-only-1-letter? [word1 word2]
+;;   (let [letter-diff (if (>= (count word1) (count word2))
+;;                       (set/difference (set word1) (set word2))
+;;                       (set/difference (set word2) (set word1)))]
+;;     (= 1 (count letter-diff))))
+
+
+;; (defn find-differ-by-1-letter [current-word coll]
+;;   (filter (fn [word]
+;;             (differs-by-only-1-letter? word current-word))
+;;         coll))
+
+;; (defn enumerate-differ-by-1-words [coll]
+;;   (into {} (map (fn [word] [word (find-differ-by-1-letter word coll)]) coll)))
+
+
+;; (defn problem82? [coll current-word]
+;;   (if (empty? coll)
+;;     true
+;;     (let [next-word (find-differ-by-1-letter current-word coll)]
+;;       (if next-word
+;;         (problem82? (remove #{next-word} coll) next-word)
+;;         false))))
+
+
+
+;; 15 : 15/2 = 7.5
+(defn is-perfect-square [num]
+  (= num (some (fn [elem] (when (<= num (* elem elem))
+                            (* elem elem)))
+               (range (inc (quot num 2))))))
+
+(defn problem74 [string]
+  (let [nums-vec (str/split string #",")]
+    (->> (filter (comp is-perfect-square #(Integer/parseInt %)) nums-vec)
+         (str/join ","))))
+
+;; (defn find-divisors [num]
+;;   (filter
+;;    (fn [ele] (= 0 (rem num ele)))
+;;    (range 1 (inc (quot num 2)))))
+
+;; (defn are-co-prime [num1 num2]
+;;   (let [
+;;         num1divisors (find-divisors num1)
+;;         num2divisors (find-divisors num2)
+;;         intersection (set/intersection (set num1divisors) (set num2divisors))]
+;;     (when (= #{1} intersection)
+;;       true)
+;;     ))
+
+;; (defn problem75 [num]
+;;   (filter
+;;    #(are-co-prime num %)
+;;    (range 1 num)))
