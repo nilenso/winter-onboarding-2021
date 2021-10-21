609c7b6f5976339a2aaae17e7fdd8fdb501bf6ba
Alisha Mohanty <alisha@nilenso.com>
2021-10-21 13:34:59 +0530

    Add set-operation representation

--- src/winter_onboarding_2021/dice_roller/alisha/core.clj
+++ src/winter_onboarding_2021/dice_roller/alisha/core.clj
@@ -4,3 +4,7 @@
 (defn dice-roll-set [values] {:type "DiceRollSet" :values (vec values)})
 
 (defn dice-notation [rolls faces] {:type "DiceNotation" :rolls rolls :faces faces})
+
+; op -> be "k", "d", "rr"
+; selector -> "" "h" "l" "<" ">"
+(defn set-operation [op selector] {:type "SetOperation" :op op :selector selector})
--- test/winter_onboarding_2021/dice_roller/alisha_test.clj
+++ test/winter_onboarding_2021/dice_roller/alisha_test.clj
@@ -9,3 +9,10 @@
 (deftest dice-notation 
   (testing "to check dice-notation return a hashmap with rolls and faces"
     (is (= (dnd-dice/dice-notation 3 4) {:type "DiceNotation" :rolls 3 :faces 4 }))))
+
+(deftest set-operation
+  (testing "to check set-operation return a hashmap with operator and selector"
+    (is (= (dnd-dice/set-operation "k" ">") {:type "SetOperation" :op "k" :selector ">"}))
+    (is (= (dnd-dice/set-operation "l" "") {:type "SetOperation" :op "l" :selector ""}))
+    (is (= (dnd-dice/set-operation "rr" "h") {:type "SetOperation" :op "rr" :selector "h"}))
+    (is (= (dnd-dice/set-operation "k" "<") {:type "SetOperation" :op "k" :selector "<"}))))
