5e0ad6d32167d8d7d63ef590a546a7f7cf2448db
Alisha Mohanty <alisha@nilenso.com>
2021-10-22 12:24:34 +0530

    Remove type property from hashmaps from datastructures

--- src/winter_onboarding_2021/dice_roller/alisha/core.clj
+++ src/winter_onboarding_2021/dice_roller/alisha/core.clj
@@ -1,16 +1,16 @@
 (ns winter-onboarding-2021.dice-roller.alisha.core)
 
 ;;Define dice-roll-set expression representation
-(defn dice-roll-set [values] {:type "DiceRollSet" :values (vec values)})
+(defn dice-roll-set [values] {:values (vec values)})
 
-(defn dice-notation [rolls faces] {:type "DiceNotation" :rolls rolls :faces faces})
+(defn dice-notation [rolls faces] {:rolls rolls :faces faces})
 
 ; op -> be "k", "d", "rr"
 ; selector -> "" "h" "l" "<" ">"
-(defn set-operation [op selector] {:type "SetOperation" :op op :selector selector})
+(defn set-operation [op selector] {:op op :selector selector})
 
 ;;define set expression with consists of dice-notation and set-operation
 (defn set-expression [dice-notation set-operation]
-  {:type "SetExpression"
-   :dice-notation dice-notation
+  {:dice-notation dice-notation
    :set-operation set-operation})
+
--- test/winter_onboarding_2021/dice_roller/alisha_test.clj
+++ test/winter_onboarding_2021/dice_roller/alisha_test.clj
@@ -4,23 +4,23 @@
 
 (deftest dice-roll-set
   (testing "to check dice-roll-set return a hashmap with values"
-    (is (= (dnd-dice/dice-roll-set [1 2 3 4]) {:type "DiceRollSet" :values [1 2 3 4]}))))
+    (is (= (dnd-dice/dice-roll-set [1 2 3 4]) {:values [1 2 3 4]}))))
 
 (deftest dice-notation 
   (testing "to check dice-notation return a hashmap with rolls and faces"
-    (is (= (dnd-dice/dice-notation 3 4) {:type "DiceNotation" :rolls 3 :faces 4 }))))
+    (is (= (dnd-dice/dice-notation 3 4) {:rolls 3 :faces 4 }))))
 
 (deftest set-operation
   (testing "to check set-operation return a hashmap with operator and selector"
-    (is (= (dnd-dice/set-operation "k" ">") {:type "SetOperation" :op "k" :selector ">"}))
-    (is (= (dnd-dice/set-operation "l" "") {:type "SetOperation" :op "l" :selector ""}))
-    (is (= (dnd-dice/set-operation "rr" "h") {:type "SetOperation" :op "rr" :selector "h"}))
-    (is (= (dnd-dice/set-operation "k" "<") {:type "SetOperation" :op "k" :selector "<"}))))
+    (is (= (dnd-dice/set-operation "k" ">") {:op "k" :selector ">"}))
+    (is (= (dnd-dice/set-operation "l" "") {:op "l" :selector ""}))
+    (is (= (dnd-dice/set-operation "rr" "h") {:op "rr" :selector "h"}))
+    (is (= (dnd-dice/set-operation "k" "<") {:op "k" :selector "<"}))))
 
 (deftest set-expression
   (testing "to check set-expression return a hashmap with dice-notation and set-operation"
-    (is (= (dnd-dice/set-expression {:type "DiceNotation" :rolls 3 :faces 4} 
-                                   {:type "SetOperation" :op "k" :selector ">"})
-           {:type "SetExpression"
-            :dice-notation {:type "DiceNotation" :rolls 3 :faces 4}
-            :set-operation {:type "SetOperation" :op "k" :selector ">"}}))))
+    (is (= (dnd-dice/set-expression {:rolls 3 :faces 4} 
+                                   {:op "k" :selector ">"})
+           {:dice-notation {:rolls 3 :faces 4}
+            :set-operation {:op "k" :selector ">"}}))))
+
