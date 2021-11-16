451b9a02352242aba7e71fe151f724a90e566180
Alisha Mohanty <alishamohanty1416@gmail.com>
2021-11-16 16:59:24 +0530

    Add aero for env variables
    Co-authored-by: Shivam Singhal <shivam@nilenso.com>

--- src/winter_onboarding_2021/fleet_management_service/core.clj
+++ src/winter_onboarding_2021/fleet_management_service/core.clj
@@ -1,7 +1,10 @@
 (ns winter-onboarding-2021.fleet-management-service.core
   (:require [ring.adapter.jetty :as raj]
-            [winter-onboarding-2021.fleet-management-service.routes :as routes])
+            [winter-onboarding-2021.fleet-management-service.routes :as routes]
+            [aero.core :refer (read-config)])
   (:gen-class))
+
+(read-config "config.edn")
 
 (defonce server (atom nil))
 
@@ -27,3 +30,4 @@
   []
   (println "this is from fleet")
   (start-server))
+
