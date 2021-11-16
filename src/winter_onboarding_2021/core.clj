6187166c9ba218f0a396a64048e13d0a4c2e42b9
Govind Krishna Joshi <govind@nilenso.com>
2021-11-16 13:36:10 +0530

    Add a step to build uberjar
    Co-authored-by: Yogiraj <yogiraj@nilenso.com>

--- src/winter_onboarding_2021/core.clj
+++ src/winter_onboarding_2021/core.clj
@@ -1,6 +1,5 @@
-(ns winter-onboarding-2021.core)
+(ns winter-onboarding-2021.core
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn -main [& args]
+  (println "Hello, World!"))
