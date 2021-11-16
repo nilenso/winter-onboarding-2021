7eaa07cd63ce236be0df3cb4c79ca4ece8f28fd8
Shivam Singhal <shivam@nilenso.com>
2021-11-16 12:41:48 +0530

    Implement starting, stopping, restarting server

--- src/winter_onboarding_2021/fleet_management_service/core.clj
+++ src/winter_onboarding_2021/fleet_management_service/core.clj
@@ -1,13 +1,28 @@
 (ns winter-onboarding-2021.fleet-management-service.core
+  (:require [ring.adapter.jetty :as raj])
   (:gen-class))
 
-(println "Hello world")
+(defonce server (atom nil))
 
 (defn hello-world [request]
   {:status 200
    :headers {"Content-Type" "text/html"}
    :body "Hello from my API!"})
 
+(defn start-server []
+  (reset! server (raj/run-jetty hello-world
+                                {:port 3000
+                                 :join? false})))
+
+(defn stop-server []
+  (when @server (.stop @server))
+  (reset! server nil))
+
+(defn restart-server []
+  (stop-server)
+  (start-server))
+
 (defn -main
   []
-  (println "this is from fleet"))
+  (println "this is from fleet")
+  (start-server))
