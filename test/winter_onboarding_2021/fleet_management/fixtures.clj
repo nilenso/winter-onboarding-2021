fb3a5271a257adeec6268e7f06555317db5421a7
Govind Krishna Joshi <govind@nilenso.com>
2021-11-23 13:41:41 +0530

    Move db config to secrets
    The commit also adds ability to read config based on ENV
    
    Co-authored-by: Alisha <alisha@nilenso.com>

--- test/winter_onboarding_2021/fleet_management/fixtures.clj
+++ test/winter_onboarding_2021/fleet_management/fixtures.clj
@@ -1,12 +1,14 @@
 (ns winter-onboarding-2021.fleet-management.fixtures
   (:require [next.jdbc :as jdbc]
             [mount.core :as mount]
+            [aero.core :refer [read-config]]
             [winter-onboarding-2021.fleet-management-service.config :as config]
             [winter-onboarding-2021.fleet-management-service.db.core :as db]))
 
 (defn config [f]
   (mount/stop #'config/config)
-  (mount/start #'config/config)
+  (mount/start-with
+   {#'config/config (read-config "config/config.edn" {:profile :test})})
   (f)
   (mount/stop #'config/config))
 
@@ -17,7 +19,7 @@
   (mount/stop #'db/db-conn))
 
 (defn clear-db [f]
-  (jdbc/execute! db/db-conn  ["TRUNCATE TABLE users"])
+  (jdbc/execute! db/db-conn  ["TRUNCATE TABLE test_users"])
   (f))
 
 ;; config doubt --> pass config via CLI?
