2891e147e16c9b1f40fdfee7e46ab15717dc7d7f
Yogiraj H <yogiraj@nilenso.com>
2021-11-25 17:44:30 +0530

    add select query for cabs

--- src/winter_onboarding_2021/fleet_management_service/models/cab.clj
+++ src/winter_onboarding_2021/fleet_management_service/models/cab.clj
@@ -1,6 +1,14 @@
 (ns winter-onboarding-2021.fleet-management-service.models.cab
   (:require [next.jdbc.sql :as sql]
-            [winter-onboarding-2021.fleet-management-service.db.core :as db]))
+            [winter-onboarding-2021.fleet-management-service.db.core :as db]
+            [next.jdbc.result-set :as rs]))
 
 (defn create [cab]
   (sql/insert! db/db-conn :cabs cab))
+
+
+;;{:builder-fn rs/as-unqualified-lower-maps}
+
+(defn select []
+  (sql/query db/db-conn ["SELECT * FROM cabs"] {:builder-fn rs/as-unqualified-lower-maps}))
+
