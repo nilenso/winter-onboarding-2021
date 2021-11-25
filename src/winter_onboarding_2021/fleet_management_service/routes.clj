ff2a7a96f361c36d254fd5c1b37fd4167fd29e91
Yogiraj H <yogiraj@nilenso.com>
2021-11-25 17:43:56 +0530

    add route for GET cabs

--- src/winter_onboarding_2021/fleet_management_service/routes.clj
+++ src/winter_onboarding_2021/fleet_management_service/routes.clj
@@ -1,11 +1,13 @@
 (ns winter-onboarding-2021.fleet-management-service.routes
   (:require [bidi.ring :as br]
             [winter-onboarding-2021.fleet-management-service.handlers.cab :as cab]
+            [ring.middleware.json :refer [wrap-json-response]]
             [winter-onboarding-2021.fleet-management-service.handler :as handler]))
 
 (def routes
   ["/" [["public" (br/->Resources {:prefix "/bootstrap"})]
-        ["cabs" {:post {"/" cab/add}
+        ["cabs" {:get {"/" (wrap-json-response cab/get-cabs)}
+                 :post {"/" cab/add}
                  "/new" (fn [_] {:status 200 :body "New cab form page"})}]
         ["healthcheck" handler/health-check]
         ["index" handler/index]
