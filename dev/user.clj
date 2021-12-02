ddfcfdd19f5e8c2427aed9235de2e3e71a89a1ee
Shivam <shivam@nilenso.com>
2021-12-02 18:23:01 +0530

    Explicitly set `tools.namespace` refresh dirs

--- dev/user.clj
+++ dev/user.clj
@@ -2,6 +2,8 @@
   (:require [mount.core :as mount]
             [clojure.tools.namespace.repl :refer [refresh]]))
 
+(clojure.tools.namespace.repl/set-refresh-dirs "src" "test")
+
 #_:clj-kondo/ignore
 (defn restart-server []
   (mount/stop)
