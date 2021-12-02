0645b77eb359f31851ca338038240bd4a5b7b509
Shivam <shivam@nilenso.com>
2021-12-02 22:11:15 +0530

    Fix test for POST /cabs handler

--- test/winter_onboarding_2021/fleet_management/handlers/cab_test.clj
+++ test/winter_onboarding_2021/fleet_management/handlers/cab_test.clj
@@ -1,5 +1,6 @@
 (ns winter-onboarding-2021.fleet-management.handlers.cab-test
   (:require [clojure.test :refer [deftest is testing use-fixtures]]
+            [clojure.string :as str]
             [winter-onboarding-2021.fleet-management-service.handlers.cab :as handlers]
             [winter-onboarding-2021.fleet-management-service.views.cab :as views]
             [winter-onboarding-2021.fleet-management-service.models.cab :as models]
@@ -10,28 +11,38 @@
 (use-fixtures :each fixtures/clear-db)
 
 (deftest add-cab
-  (testing "POST  /cabs/ endpoint with valid cab data, shoudld redirect to '/cabs/new?success=true' "
+  (testing "POST /cabs/ endpoint with valid cab data, shoudld redirect to '/cabs/<<uuid of new cab>> "
+    (let [response (handlers/create {:multipart-params
+                                     {:name "Test cab"
+                                      :licence_plate "KA20X1234"
+                                      :distance_travelled "1223"}})]
+      (is (= 302 (:status response)))
+
+      (is (= {:success true
+              :style-class "alert alert-success"
+              :message "Cab added successfully!"}
+             (:flash response)))
+
+      (is (= true (-> response
+                      (get-in [:headers "Location"])
+                      (str/split #"/") ; Could be better if we use regex
+                      (nth 2)
+                      count
+                      (= 36))))
+
+      (is (= "" (:body response))))))
+
+(testing "POST /cabs/ endpoint with invalid cab data, should redirect to '/cabs/new"
+  (let [invalid-cab {:name "Test cab"
+                     :licence_plate "KA20X1234"}]
     (is (= {:status 302
-            :flash {:success true
-                    :style-class "alert alert-success"
-                    :message "Cab added successfully!"}
+            :flash {:error true
+                    :style-class "alert alert-danger"
+                    :message "Could not add cab, try again!"
+                    :data invalid-cab}
             :headers {"Location" "/cabs/new"}
             :body ""}
-           (handlers/create {:multipart-params {:name "Test cab"
-                                                :licence_plate "KA20X1234"
-                                                :distance_travelled "1223"}}))))
-
-  (testing "POST /cabs/ endpoint with invalid cab data, should redirect to '/cabs/new?error=true"
-    (let [invalid-cab {:name "Test cab"
-                       :licence_plate "KA20X1234"}]
-      (is (= {:status 302
-              :flash {:error true
-                      :style-class "alert alert-danger"
-                      :message "Could not add cab, try again!"
-                      :data invalid-cab}
-              :headers {"Location" "/cabs/new"}
-              :body ""}
-             (handlers/create {:multipart-params invalid-cab}))))))
+           (handlers/create {:multipart-params invalid-cab})))))
 
 (deftest view-single-cab
   (testing "Should return 200 code with the HTML of the single cab details view"
