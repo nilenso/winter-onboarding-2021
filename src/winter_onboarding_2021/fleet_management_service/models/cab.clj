70790b2ad55644adf5113e3751df8e53a2272c9e
Yogiraj H <yogiraj@nilenso.com>
2021-12-03 18:42:07 +0530

    Saperate model and db test fns & use config-getter
    Co-authored-by: Shivam Singhal <shivam@nilenso.com>

--- src/winter_onboarding_2021/fleet_management_service/models/cab.clj
+++ src/winter_onboarding_2021/fleet_management_service/models/cab.clj
@@ -1,11 +1,12 @@
 (ns winter-onboarding-2021.fleet-management-service.models.cab
-  (:require [winter-onboarding-2021.fleet-management-service.db.cab :as cab-db]))
+  (:require [winter-onboarding-2021.fleet-management-service.db.cab :as cab-db]
+            [winter-onboarding-2021.fleet-management-service.config :as config]))
 
 (defn create [cab]
   (cab-db/create cab))
 
-(defn select 
-  ([] (cab-db/select 0 10))
+(defn select
+  ([] (cab-db/select 0 (config/get-page-size)))
   ([offset limit] (cab-db/select offset limit)))
 
 (defn cabs-count []
--- test/winter_onboarding_2021/fleet_management/db/cab_test.clj
+++ test/winter_onboarding_2021/fleet_management/db/cab_test.clj
@@ -1,9 +1,8 @@
 (ns winter-onboarding-2021.fleet-management.db.cab-test
   (:require [clojure.test :refer [deftest is testing use-fixtures]]
-            [winter-onboarding-2021.fleet-management-service.models.cab :as cab]
+            [winter-onboarding-2021.fleet-management-service.db.cab :as cab-db]
             [winter-onboarding-2021.fleet-management.fixtures :as fixtures]
-            [winter-onboarding-2021.fleet-management.factories :as factories])
-  (:import [org.postgresql.util PSQLException]))
+            [winter-onboarding-2021.fleet-management.factories :as factories]))
 
 (use-fixtures :once fixtures/config fixtures/db-connection)
 (use-fixtures :each fixtures/clear-db)
@@ -11,24 +10,6 @@
 (defn vectors-contain-same-elements? [x y]
   (= (set x) (set y)))
 
-(deftest create-cab
-  (testing "Should add a cab"
-    (let [created-cab (cab/create {:licence_plate "HR20X 6710"
-                                   :name "Maruti Celerio"
-                                   :distance-travelled 2333})]
-      (is (= #:cabs{:licence-plate "HR20X 6710"
-                    :name "Maruti Celerio"
-                    :distance-travelled 2333}
-             (dissoc created-cab
-                     :cabs/id
-                     :cabs/created-at
-                     :cabs/updated-at)))))
-
-  (testing "Should fail to create a cab because of invalid cab"
-    (is (thrown-with-msg?
-         PSQLException
-         #"null value in column \"licence_plate\" of relation \"cabs\""
-         (cab/create {:name "Kia"})))))
 
 (defn dissoc-cab [created-cab]
   (dissoc created-cab
@@ -38,23 +19,25 @@
 
 (deftest list-cabs
   (testing "Shoudld return a list of cabs"
-    (let [cabs-list (factories/create-cabs 3)]
-      (doall (map cab/create cabs-list))
+    (let [cabs-list (factories/create-cabs 3)
+          offset 0
+          page-length 10]
+      (doall (map cab-db/create cabs-list))
       (is (= cabs-list
-             (map dissoc-cab (cab/select)))))))
+             (map dissoc-cab (cab-db/select offset page-length)))))))
 
 (deftest pagination
   (let [cabs (factories/create-cabs 12)]
-    (doall (map cab/create cabs))
+    (doall (map cab-db/create cabs))
     (testing "Should return last 2 cabs from a table of 12 cabs"
       (let [offset 10
             limit 2]
         (is (vectors-contain-same-elements?  (take limit (reverse cabs))
                                              (map dissoc-cab
-                                                  (cab/select offset limit))))))
+                                                  (cab-db/select offset limit))))))
     (testing "Should return first 10 cabs from 12 cabs"
       (let [offset 0
             limit 10]
         (is (vectors-contain-same-elements?  (take limit cabs)
                                              (map dissoc-cab
-                                                  (cab/select offset limit))))))))
+                                                  (cab-db/select offset limit))))))))
