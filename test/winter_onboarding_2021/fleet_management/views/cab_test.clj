1a3ff6e549159fefc4c5cc0727829487bf00b18c
Yogiraj H <yogiraj@nilenso.com>
2021-12-15 12:44:07 +0530

    Move html5 to wrap-layout from layout/application
    Co-authored-by: Shivam Singhal <shivam@nilenso.com>

--- test/winter_onboarding_2021/fleet_management/views/cab_test.clj
+++ test/winter_onboarding_2021/fleet_management/views/cab_test.clj
@@ -9,45 +9,44 @@
 (deftest add-cab-view
   (testing "Should have alert success bubble after adding a valid cab"
     (let [success-msg "Cab added successfully!"
-          output-html (layout/application
-                       {:flash {:success true
-                                :style-class "alert alert-success"
-                                :message success-msg}}
-                       "Add a cab"
-                       (cab/cab-form {:licence-plate "" :name "" :distance-travelled ""}))]
-      (is (str/includes?
-           output-html
-           (format
-            "<div class=\"alert alert-success\">%s</div>"
-            success-msg)))))
+          hiccup-output (layout/application
+                         {:flash {:success true
+                                  :style-class "alert alert-success"
+                                  :message success-msg}}
+                         "Add a cab"
+                         (cab/cab-form {:licence-plate "" :name "" :distance-travelled ""}))]
+      (is (= success-msg
+             (hf/hiccup-text (hf/hiccup-find [:.alert.alert-success] hiccup-output))))))
 
   (testing "Should have alert error bubble after trying to add an invalid cab"
     (let [error-msg "Could not add cab, try again!"
-          output-html (layout/application
-                       {:flash {:error true
-                                :style-class "alert alert-danger"
-                                :message error-msg}}
-                       "Add a cab"
-                       (cab/cab-form {:licence-plate "" :name "" :distance-travelled ""}))]
-      (is (str/includes?
-           output-html
-           (format "<div class=\"alert alert-danger\">%s</div>" error-msg))))))
+          hiccup-output (layout/application
+                         {:flash {:error true
+                                  :style-class "alert alert-danger"
+                                  :message error-msg}}
+                         "Add a cab"
+                         (cab/cab-form {:licence-plate "" :name "" :distance-travelled ""}))]
+      (is (= error-msg
+             (hf/hiccup-text (hf/hiccup-find [:.alert.alert-danger] hiccup-output)))))))
 
 (deftest view-single-cab
   (testing "Should have the details like name, licence plate and distance travelled of single cab"
     (let [cab #:cabs{:name "Maruti Cab"
                      :licence-plate "HR20A 1234"
                      :distance-travelled 12333}
-          output-html (layout/application
+          hiccup-output (layout/application
                        {} ; request is empty
                        (format "Cab %s" (:name cab))
                        (cab/cab cab))]
       
-      (is (str/includes? output-html (:cabs/name cab)))
+      (is (= (hf/hiccup-text (hf/hiccup-find [:h2] hiccup-output))
+             (:cabs/name cab)))
 
-      (is (str/includes? output-html (:cabs/licence-plate cab)))
+      (is (= (hf/hiccup-text (first (hf/hiccup-find [:h3] hiccup-output))) 
+             (:cabs/licence-plate cab)))
 
-      (is (str/includes? output-html (str (:cabs/distance-travelled cab)))))))
+      (is (= (hf/hiccup-text (second (hf/hiccup-find [:h3] hiccup-output)))
+           (str (:cabs/distance-travelled cab)))))))
 
 (deftest update-cab-form
  (testing "Should have form labels name, licence plate and distance travelled"
@@ -60,9 +59,3 @@
      (is (str/includes? (html5 output-view) (:cabs/name cab)))
      (is (str/includes? (html5 output-view) (str (:cabs/distance-travelled cab))))
      (is (str/includes? (html5 output-view) (:cabs/licence-plate cab))))))
-
-(deftest contains-navbar
-  (testing "Should contain navbar"
-    (is (= true
-           (str/includes? (layout/application {} "Test Page" "")
-                          "nav-item")))))
--- test/winter_onboarding_2021/fleet_management/views/layout_test.clj
+++ test/winter_onboarding_2021/fleet_management/views/layout_test.clj
@@ -1,13 +1,15 @@
 (ns winter-onboarding-2021.fleet-management.views.layout-test
   (:require [clojure.test :refer [deftest is testing]]
             [clojure.string :as string]
+            [hiccup.page :refer [html5]]
+            [hiccup-find.core :as hf]
             [winter-onboarding-2021.fleet-management-service.views.layout :as layout]))
 
 (deftest application
   (testing "Should contain bootstrap files & the page title"
-    (let [output (layout/application {}
-                                     "Test Title"
-                                     [:div])]
+    (let [output (html5 (layout/application {}
+                                            "Test Title"
+                                            [:div]))]
       (is (string/includes? output "Test Title"))
       (is (string/includes? output
                             "<link href=\"/public/css/bootstrap.min.css\" rel=\"stylesheet\" type=\"text/css\">"))
@@ -15,3 +17,8 @@
                             "<script src=\"/public/js/jquery-3.6.0.min.js\" type=\"text/javascript\"></script>"))
       (is (string/includes? output
                             "<script src=\"/public/js/bootstrap.bundle.min.js\" type=\"text/javascript\"></script>")))))
+
+(deftest contains-navbar
+  (testing "Should contain navbar"
+    (is (= (count (hf/hiccup-find [:header] (layout/application {} "Test Page" "")))
+           1))))
