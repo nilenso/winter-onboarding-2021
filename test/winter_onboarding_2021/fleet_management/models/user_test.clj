b08bfdec74cf605761146c0bbe59c5cc237e2626
Yogiraj H <yogiraj@nilenso.com>
2021-12-15 18:04:35 +0530

    Implement create-user
    Co-authored-by: Alisha Mohanty <alishamohanty1416@gmail.com>
    Co-authored-by: Shivam Singhal <shivam@nilenso.com>
    
    Co-authored-by: Yogiraj H <yogiraj@nilenso.com>

--- test/winter_onboarding_2021/fleet_management/models/user_test.clj
+++ test/winter_onboarding_2021/fleet_management/models/user_test.clj
@@ -1,22 +1,23 @@
-(ns winter-onboarding-2021.fleet-management.db.user-test
+(ns winter-onboarding-2021.fleet-management.models.user-test
   (:require [clojure.test :refer [deftest is testing use-fixtures]]
-            [winter-onboarding-2021.fleet-management-service.db.user :as user-db]
+            [winter-onboarding-2021.fleet-management-service.models.user :as user-model]
             [winter-onboarding-2021.fleet-management-service.db.core :as db-core]
             [winter-onboarding-2021.fleet-management.fixtures :as fixtures]))
 
 (use-fixtures :once fixtures/config fixtures/db-connection)
 (use-fixtures :each fixtures/clear-db)
 
-(deftest create-user
-  (testing "Should create a user in the users table"
+(deftest create-cab
+  (testing "Should create a cab"
     (let [user {:name "Harry Potter"
                 :role "admin"
                 :email "harry@hogwarts.edu"
                 :password "hermione@123"}
-          _ (user-db/create user)]
+          _ (user-model/create user)]
       (is (= #:users{:name "Harry Potter"
-                    :role "admin"
-                    :email "harry@hogwarts.edu"
-                    :password "hermione@123"}
+                     :role "admin"
+                     :email "harry@hogwarts.edu"
+                     :password "hermione@123"}
              (dissoc (first (db-core/query! ["SELECT * FROM users;"]))
                      :users/id))))))
+
---
+++ test/winter_onboarding_2021/fleet_management/views/core_test.clj
@@ -0,0 +1,10 @@
+(ns winter-onboarding-2021.fleet-management.views.core-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [hiccup-find.core :as hf]
+            [winter-onboarding-2021.fleet-management-service.views.core :as core]))
+
+
+(deftest index-view
+  (testing "Should return hello string"
+    (is (= "Hello Hiccup & Bootstrap"
+           (hf/hiccup-text (core/index))))))
