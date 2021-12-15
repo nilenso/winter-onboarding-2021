cca9030766ddaac667bbf0af1b063c5b837c55bb
Shivam <shivam@nilenso.com>
2021-12-15 14:44:17 +0530

    Add migrations for User model
    Co-authored-by: Yogiraj H <yogiraj@nilenso.com>

---
+++ resources/migrations/20211215052838-create-user.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users(
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name TEXT NOT NULL,
+    role TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
