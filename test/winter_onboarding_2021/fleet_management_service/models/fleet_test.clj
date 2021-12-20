811d7260963ac568458de97cef45db835a33d40d
Alisha Mohanty <alishamohanty1416@gmail.com>
2021-12-20 18:01:58 +0530

    Add db-query to create fleet

---
+++ test/winter_onboarding_2021/fleet_management_service/models/fleet_test.clj
@@ -0,0 +1,21 @@
+(ns winter-onboarding-2021.fleet-management-service.models.fleet-test
+  (:require [clojure.test :refer [deftest is testing use-fixtures]]
+            [winter-onboarding-2021.fleet-management-service.models.fleet :as fleet-model]
+            [winter-onboarding-2021.fleet-management-service.db.user :as user-db]
+            [winter-onboarding-2021.fleet-management-service.fixtures :as fixtures]))
+
+(use-fixtures :once fixtures/config fixtures/db-connection)
+(use-fixtures :each fixtures/clear-db)
+
+(deftest create-fleet
+  (testing "Should create a fleet"
+    (let [user {:name "Hermione Granger"
+                :role "admin"
+                :email "hermione@hogwarts.edu"
+                :password "weasley&potter@123"}
+          user-id (:users/id (user-db/create user))
+          fleet {:name "Azkaban Fleet 1"
+                 :created-by user-id}]
+      (is (= #:fleets{:name "Azkaban Fleet 1"
+                      :created-by user-id} (select-keys (fleet-model/create fleet)
+                                                        [:fleets/name :fleets/created-by]))))))
