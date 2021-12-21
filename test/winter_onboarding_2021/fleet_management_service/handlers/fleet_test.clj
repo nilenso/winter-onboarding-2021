d37114e2ed65be4764647edf46a7ccd3774a59b4
Alisha Mohanty <alishamohanty1416@gmail.com>
2021-12-21 19:10:30 +0530

    Add create-fleet handler

--- test/winter_onboarding_2021/fleet_management_service/handlers/fleet_test.clj
+++ test/winter_onboarding_2021/fleet_management_service/handlers/fleet_test.clj
@@ -1,6 +1,6 @@
-(ns winter-onboarding-2021.fleet-management-service.handlers.fleet-test
+(ns winter-onboarding-2021.fleet-management.handlers.fleet-test
   (:require [clojure.test :refer [deftest is testing use-fixtures]]
-            [winter-onboarding-2021.fleet-management-service.fixtures :as fixtures]
+            [winter-onboarding-2021.fleet-management.fixtures :as fixtures]
             [winter-onboarding-2021.fleet-management-service.handlers.fleet :as fleet-handler]
             [winter-onboarding-2021.fleet-management-service.db.core :as db-core]
             [winter-onboarding-2021.fleet-management-service.db.user :as user-db]))
@@ -25,16 +25,4 @@
       (is (= 302 (:status response)))
       (is (= #:fleets{:name "Goo fleet"
                       :created-by user-id} (select-keys inserted-fleet 
-                                                        [:fleets/name :fleets/created-by])))
-      (is (= {:success true
-              :style-class "alert alert-success"
-              :message "Fleet created successfully!"} (:flash response)))))
-  
-  (testing "Should not create a fleet if the admin is not logged-in"
-    (let [request {:form-params {:name "Boo fleet"}}
-          response (fleet-handler/create-fleet request)]
-      (is (= 302 (:status response)))
-      (is (= {"Location" "/fleets/new"} (:headers response) ))
-      (is (= {:error true
-              :style-class "alert alert-danger"
-              :message "Could not create fleet, try again!"} (:flash response))))))
+                                                        [:fleets/name :fleets/created-by]))))))
