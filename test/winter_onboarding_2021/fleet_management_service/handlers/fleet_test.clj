8ac632571b6d3d4cde9bd5bbe44daef8742744d5
Alisha Mohanty <alishamohanty1416@gmail.com>
2021-12-21 19:10:30 +0530

    Add create-fleet handler

---
+++ test/winter_onboarding_2021/fleet_management_service/handlers/fleet_test.clj
@@ -0,0 +1,28 @@
+(ns winter-onboarding-2021.fleet-management-service.handlers.fleet-test
+  (:require [clojure.test :refer [deftest is testing use-fixtures]]
+            [winter-onboarding-2021.fleet-management-service.fixtures :as fixtures]
+            [winter-onboarding-2021.fleet-management-service.handlers.fleet :as fleet-handler]
+            [winter-onboarding-2021.fleet-management-service.db.core :as db-core]
+            [winter-onboarding-2021.fleet-management-service.db.user :as user-db]))
+
+(use-fixtures :once fixtures/config fixtures/db-connection)
+(use-fixtures :each fixtures/clear-db)
+
+(deftest create-fleet
+  (testing "Should create a fleet by an admin"
+    (let [user {:name "foo bar"
+                :role "admin"
+                :email "foobar@gmail.com"
+                :password "foo"}
+          user-id (:users/id (user-db/create user))
+          request {:form-params {:name "Goo fleet"}
+                   :user #:users{:id user-id
+                                 :name "foo bar"
+                                 :role "admin"
+                                 :email "foobar@gmail.com"}}
+          response (fleet-handler/create-fleet request)
+          inserted-fleet (first (db-core/query! ["SELECT * FROM FLEETS"]))]
+      (is (= 302 (:status response)))
+      (is (= #:fleets{:name "Goo fleet"
+                      :created-by user-id} (select-keys inserted-fleet 
+                                                        [:fleets/name :fleets/created-by]))))))
