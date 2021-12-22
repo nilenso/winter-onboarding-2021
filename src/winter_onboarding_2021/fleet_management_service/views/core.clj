cb653a4af9a058d07e4005c068657c139fd56681
Shivam <shivam@nilenso.com>
2021-12-22 13:48:52 +0530

    Use a common form input component
    Co-authored-by: Yogiraj H <yogiraj@nilenso.com>

--- src/winter_onboarding_2021/fleet_management_service/views/core.clj
+++ src/winter_onboarding_2021/fleet_management_service/views/core.clj
@@ -1,5 +1,14 @@
-(ns winter-onboarding-2021.fleet-management-service.views.core)
+(ns winter-onboarding-2021.fleet-management-service.views.core
+  (:require [camel-snake-kebab.core :as csk]))
 
 (defn index []
   [:div {:id "content"}
    [:h1 {:class "text-success"} "Hello Hiccup & Bootstrap"]])
+
+(defn labelled-text-input [label & args]
+  (let [options (apply hash-map args)
+        name (csk/->kebab-case-string label)]
+    [:div
+     [:label {:for label :class "form-label"} label]
+     [:input (merge {:class "form-control" :type "text" :name name}
+                    options)]]))
--- src/winter_onboarding_2021/fleet_management_service/views/user.clj
+++ src/winter_onboarding_2021/fleet_management_service/views/user.clj
@@ -1,15 +1,27 @@
-(ns winter-onboarding-2021.fleet-management-service.views.user)
+(ns winter-onboarding-2021.fleet-management-service.views.user
+  (:require [winter-onboarding-2021.fleet-management-service.views.core :as core-views]))
 
 
 (defn signup-form []
   [:form {:action "/users/signup" :method "POST"}
    [:div {:class "mb-3" :id "email"}
-    [:label {:for "signup-email-input" :class "form-label"} "Email address"]
-    [:input {:required "required" :id "signup-email-input" :class "form-control" :type "email", :name "email"}]]
+    (core-views/labelled-text-input
+     "Email address"
+     :required true
+     :id "signup-email-input"
+     :name "email"
+     :type "email")]
    [:div {:class "mb-3" :id "name"}
-    [:label {:for "signup-name-input" :class "form-label"} "Name"]
-    [:input {:required "required" :id "signup-name-input" :class "form-control" :name "name"}]]
+    (core-views/labelled-text-input
+     "Name"
+     :required true
+     :id "signup-name-input"
+     :name "name")]
    [:div {:class "mb-3" :id "password"}
-    [:label {:for "signup-pwd-input" :class "form-label"} "Password"]
-    [:input  {:required "required" :id "signup-pwd-input" :class "form-control" :type "password", :name "password"}]]
+    (core-views/labelled-text-input
+     "Password"
+     :required true
+     :id "signup-pwd-input"
+     :type "password"
+     :name "password")]
    [:button {:type "submit" :class "btn btn-primary"} "Submit"]])
