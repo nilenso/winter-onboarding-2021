25be5bf324f67585b6308ce3bbdf03582e1485b4
Shivam <shivam@nilenso.com>
2022-01-03 16:10:01 +0530

    Asset on `action` of login form

--- test/winter_onboarding_2021/fleet_management/views/user_test.clj
+++ test/winter_onboarding_2021/fleet_management/views/user_test.clj
@@ -5,22 +5,27 @@
 
 (deftest signup-form
   (testing "Should return inputs for email,name and password"
-    (is (= 4
-           (count (hf/hiccup-find [:input] (view/signup-form)))))
-    (is (= "Email address"
-           (hf/hiccup-text (hf/hiccup-find [:#email] (view/signup-form)))))
-    (is (= "Name"
-           (hf/hiccup-text (hf/hiccup-find [:#name] (view/signup-form)))))
-    (is (= "Password"
-           (hf/hiccup-text (hf/hiccup-find [:#password] (view/signup-form)))))))
+    (let [signup-form-output (view/signup-form)]
+      (is (= 4
+             (count (hf/hiccup-find [:input] signup-form-output))))
+      (is (= "Email address"
+             (hf/hiccup-text (hf/hiccup-find [:#email] signup-form-output))))
+      (is (= "Name"
+             (hf/hiccup-text (hf/hiccup-find [:#name] signup-form-output))))
+      (is (= "Password"
+             (hf/hiccup-text (hf/hiccup-find [:#password] signup-form-output)))))))
 
 (deftest login-form
   (testing "Should return inputs for email and password"
-    (is (= 2
-           (count (hf/hiccup-find [:input] (view/login-form)))))
-    (is (= "Login"
-           (hf/hiccup-text (hf/hiccup-find [:h1] (view/login-form)))))
-    (is (= "Email address"
-           (hf/hiccup-text (hf/hiccup-find [:#email] (view/login-form)))))
-    (is (= "Password"
-           (hf/hiccup-text (hf/hiccup-find [:#password] (view/login-form)))))))
+    (let [login-form-output (view/login-form)]
+      (is (= 2
+             (count (hf/hiccup-find [:input] login-form-output))))
+
+      (is (= nil (:action (second (first (hf/hiccup-find [:form] login-form-output))))))
+
+      (is (= "Login"
+             (hf/hiccup-text (hf/hiccup-find [:h1] login-form-output))))
+      (is (= "Email address"
+             (hf/hiccup-text (hf/hiccup-find [:#email] login-form-output))))
+      (is (= "Password"
+             (hf/hiccup-text (hf/hiccup-find [:#password] login-form-output)))))))
