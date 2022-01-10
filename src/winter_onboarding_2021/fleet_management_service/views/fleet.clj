94aa0d4d0c5f0d9c4773544a1c69b1b4107a8751
Shivam <shivam@nilenso.com>
2022-01-10 12:40:36 +0530

    Add view for listing fleets

--- src/winter_onboarding_2021/fleet_management_service/views/fleet.clj
+++ src/winter_onboarding_2021/fleet_management_service/views/fleet.clj
@@ -1,4 +1,6 @@
-(ns winter-onboarding-2021.fleet-management-service.views.fleet)
+(ns winter-onboarding-2021.fleet-management-service.views.fleet
+  (:require [clojure.string :as string]
+            [winter-onboarding-2021.fleet-management-service.utils :as utils]))
 
 (defn create-fleet []
   [:div {:id "content"}
@@ -7,3 +9,30 @@
     [:div [:label {:for "name" :class "form-label"} "Fleet Name"]
      [:input {:class "form-control" :type "text" :name "name" :id "name" :value ""}]]
     [:button {:type "submit" :class "btn btn-primary"} "Submit"]]])
+
+(def headers
+  [{:label "Name" :value :fleets/name}
+   {:label "Managers" :value :managers}
+   {:label "Created At" :value :fleets/created-at}])
+
+(defn managers-list [managers]
+  (string/join ", " (map #(:users/name %) managers)))
+
+(defn fleet-row [row]
+  [:tr
+   [:td [:a {:href (str "/fleets/" (:fleets/id row))
+             :class "link-primary"}
+         (:fleets/name row)]]
+   [:td (managers-list (:managers row))]
+   [:td (utils/format-date (:fleets/created-at row))]])
+
+(defn show-fleets [fleets-with-managers page-num show-next-page?]
+  (let [next-page-query (str "?page=" (inc page-num))]
+    [:div
+     [:table {:class "table table-primary min-vh-40"}
+      [:thead [:tr (map (fn [header] [:th (:label header)])
+                        headers)]]
+      [:tbody (map fleet-row fleets-with-managers)]]
+     [:div {:class "text-end"}
+      (if show-next-page?
+        [:a {:id "cab-next-page" :href next-page-query}  "Next Page > "] ())]]))
--- test/winter_onboarding_2021/fleet_management_service/views/fleet_test.clj
+++ test/winter_onboarding_2021/fleet_management_service/views/fleet_test.clj
@@ -1,10 +1,16 @@
-(ns winter-onboarding-2021.fleet-management.views.fleet-test
-  (:require [clojure.test :refer [deftest is testing]]
+(ns winter-onboarding-2021.fleet-management-service.views.fleet-test
+  (:require [clojure.test :refer [deftest is testing use-fixtures]]
             [hiccup-find.core :as hf]
-            [winter-onboarding-2021.fleet-management-service.views.fleet :as views]))
+            [winter-onboarding-2021.fleet-management-service.fixtures :as fixtures]
+            [winter-onboarding-2021.fleet-management-service.views.fleet :as views]
+            [winter-onboarding-2021.fleet-management-service.db.fleet-test :as fleet-db-test]))
+
+(use-fixtures :once fixtures/config fixtures/db-connection)
+(use-fixtures :each fixtures/clear-db)
 
 (defn attributes [element]
-  (second element))
+  (when (map? (second element))
+    (second element)))
 
 (deftest create
   (testing "Should return us a form asking for the fleet name"
@@ -16,3 +22,21 @@
       
       (is (= "name" (:name (attributes input))))
       (is (= "" (:value (attributes input)))))))
+
+(deftest show-fleets
+  (testing "Should return us a list of hiccup divs with fleet data"
+    (let [{:keys [fleets]} (fleet-db-test/seed-user-fleets-db 5)
+          fleets-with-managers (map #(assoc %
+                                            :fleets/created-at (java.util.Date.)
+                                            :managers
+                                            [{:users/name "Manager 1"}])
+                                    fleets)
+          lists-view (views/show-fleets fleets-with-managers 1 false)
+          headers (hf/hiccup-find [:thead :tr :th] lists-view)
+          fleets-list (hf/hiccup-find [:tbody :tr] lists-view)]
+      
+      (is (= "Name" (second (first headers))))
+      (is (= "Managers" (second (second headers))))
+      (is (= "Created At" (second (nth headers 2))))
+      
+      (is (= 5 (count fleets-list))))))
