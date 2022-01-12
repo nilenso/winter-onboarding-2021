87de81cc451dcc0a128710cd2bef939b4c52c0d8
Shivam <shivam@nilenso.com>
2022-01-12 11:58:26 +0530

    Add spec & factory for organisation
    Co-authored-by: Alisha Mohanty <alisha@nilenso.com>

--- src/winter_onboarding_2021/fleet_management_service/specs.clj
+++ src/winter_onboarding_2021/fleet_management_service/specs.clj
@@ -147,3 +147,27 @@
                 :invites/valid-until
                 :invites/role
                 :invites/is-active]))
+
+(s/def ::fleet-form
+  (s/keys :req-un [:fleets/name
+                   :fleets/created-by]))
+
+
+; Organisations
+(s/def :organisations/id uuid?)
+(s/def :organisations/name (s/and string? not-empty))
+(s/def :organisations/created-by uuid?)
+(s/def :organisations/created-at inst?)
+(s/def :organisations/updated-at inst?)
+
+(s/def ::organisations
+  (s/keys :req [:organisations/id
+                :organisations/name
+                :organisations/created-by]))
+
+(s/def ::organisations-all-attr
+       (s/keys :req [:organisations/id
+                     :organisations/name
+                     :organisations/created-by
+                     :organisations/created-at
+                     :organisations/updated-at]))
