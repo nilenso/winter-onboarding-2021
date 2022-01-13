59bee60223a158504ac300b4319940e985ac60f1
Shivam <shivam@nilenso.com>
2022-01-13 15:23:54 +0530

    Refactor tests to use new `create` factory
    The previous factories required generating the mock data and then writing another line for inserting them in database.
    
    New factory `create` abstracts that part out. They now just take a spec gen & then genrates + inserts data in DB
    
    Co-authored-by: Govind Krishna Joshi <govind@nilenso.com>

--- test/winter_onboarding_2021/fleet_management_service/models/user_test.clj
+++ test/winter_onboarding_2021/fleet_management_service/models/user_test.clj
@@ -1,39 +1,33 @@
 (ns winter-onboarding-2021.fleet-management-service.models.user-test
   (:require [clojure.test :refer [deftest is testing use-fixtures]]
             [crypto.password.bcrypt :as password]
+            [winter-onboarding-2021.fleet-management-service.factories :as factories]
             [winter-onboarding-2021.fleet-management-service.models.user :as user-model]
             [winter-onboarding-2021.fleet-management-service.fixtures :as fixtures]))
 
 (use-fixtures :once fixtures/config fixtures/db-connection)
 (use-fixtures :each fixtures/clear-db)
 
-(deftest create-cab
-  (testing "Should create a cab"
-    (let [user {:users/name "Harry Potter"
-                :users/role "admin"
-                :users/email "harry@hogwarts.edu"
-                :users/password "hermione@123"}
+(defn select-relevant-keys [user]
+  (select-keys user [:users/name :users/email :users/password :users/role]))
+
+(deftest create-user
+  (testing "Should create a user"
+    (let [user #:users{:name "Harry Potter"
+                       :role "admin"
+                       :email "harry@hogwarts.edu"
+                       :password "hermione@123"}
           _ (user-model/create user)]
-      (is (= #:users{:name "Harry Potter"
-                     :role "admin"
-                     :email "harry@hogwarts.edu"
-                     :password "hermione@123"}
-             (select-keys (first (user-model/find-by-keys {:users/email (:users/email user)}))
-                          [:users/name :users/role :users/email :users/password]))))))
+      (is (= (select-relevant-keys user)
+             (select-relevant-keys (first (user-model/find-by-keys
+                                           {:users/email (:users/email user)}))))))))
 
 (deftest find-by-keys
   (testing "Should return a user given a key-map(properties)"
-    (let [user {:users/name "Harry Potter"
-                :users/role "admin"
-                :users/email "harry@hogwarts.edu"
-                :users/password "hermione@123"}
-          _ (user-model/create user)]
-      (is (= #:users{:name "Harry Potter"
-                     :role "admin"
-                     :email "harry@hogwarts.edu"
-                     :password "hermione@123"}
-             (select-keys (first (user-model/find-by-keys {:users/email (:users/email user)}))
-                          [:users/name :users/role :users/email :users/password]))))))
+    (let [user (factories/admin)]
+      (is (= (select-relevant-keys user)
+             (select-relevant-keys (first (user-model/find-by-keys
+                                           {:users/email (:users/email user)}))))))))
 
 (deftest authenticate
   (testing "Correct login credentials, should return us user data"
