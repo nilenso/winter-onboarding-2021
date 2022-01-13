286088b2d0096c39ca0e332d9c3961b57e76a75c
Shivam <shivam@nilenso.com>
2022-01-13 13:46:45 +0530

    Add view for organisation
    Co-authored-by: Alisha Mohanty <alisha@nilenso.com>

---
+++ test/winter_onboarding_2021/fleet_management_service/views/organisation_test.clj
@@ -0,0 +1,17 @@
+(ns winter-onboarding-2021.fleet-management-service.views.organisation-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [hiccup-find.core :as hf]
+            [winter-onboarding-2021.fleet-management-service.utils :as utils]
+            [winter-onboarding-2021.fleet-management-service.views.organisation :as org-views]))
+
+(deftest create
+  (testing "Should show the view to an admin for creating a fleet"
+    (let [create-view (org-views/create)
+          form (first (hf/hiccup-find [:form] create-view))
+          name-input (first (hf/hiccup-find [:form :input] create-view))
+          name-input-attrs (utils/hiccup-attrs name-input)]
+      (is (= "/organisations/new" (:action (utils/hiccup-attrs form))))
+      (is (= "POST" (:method (utils/hiccup-attrs form))))
+
+      (is (= "name" (:name name-input-attrs)))
+      (is (= true (:required name-input-attrs))))))
