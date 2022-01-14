73d212a175d535d19e4b6c679c51d63a05a679b3
Shivam <shivam@nilenso.com>
2022-01-14 13:30:59 +0530

    Modify `wrap-layout` to accomodate http responses
    Co-authored-by: Govind Krishna Joshi <govind@nilenso.com>

--- src/winter_onboarding_2021/fleet_management_service/routes.clj
+++ src/winter_onboarding_2021/fleet_management_service/routes.clj
@@ -1,23 +1,13 @@
 (ns winter-onboarding-2021.fleet-management-service.routes
   (:require [bidi.ring :as br]
-            [ring.util.response :as response]
+            
             [ring.middleware.json :refer [wrap-json-response]]
-            [hiccup.page :refer [html5]]
             [winter-onboarding-2021.fleet-management-service.handlers.cab :as cab-handlers]
             [winter-onboarding-2021.fleet-management-service.handlers.fleet :as fleet-handlers]
-            [winter-onboarding-2021.fleet-management-service.views.layout :as layout]
             [winter-onboarding-2021.fleet-management-service.handlers.core :as handler]
             [winter-onboarding-2021.fleet-management-service.handlers.user :as user-handlers]
             [winter-onboarding-2021.fleet-management-service.handlers.dashboard :as dashboard-handlers]
             [winter-onboarding-2021.fleet-management-service.handlers.organisation :as org-handlers]))
-
-(defn wrap-layout [handler]
-  (fn [request]
-    (let [data (handler request)]
-      (response/response (html5 (layout/application
-                                 request
-                                 (:title data)
-                                 (:content data)))))))
 
 (defn authentication-required [handler allowed-roles]
   (fn [request]
@@ -29,29 +19,29 @@
 
 (def routes
   ["/" [["public" {:get (br/->Resources {:prefix "/bootstrap"})}]
-        ["cabs" {"" {:get (authentication-required (wrap-layout cab-handlers/get-cabs) #{:admin :manager})
+        ["cabs" {"" {:get (authentication-required cab-handlers/get-cabs #{:admin :manager})
                      :post cab-handlers/create}
-                 "/new" {:get (authentication-required  (wrap-layout cab-handlers/new) #{:admin :manager})}
-                 "/delete" {:post (authentication-required  (wrap-layout cab-handlers/delete) #{:admin :manager})}
-                 ["/" :slug] {"/edit" {:get (wrap-layout cab-handlers/update-cab-view)}
-                              :get (wrap-layout cab-handlers/view-cab)
+                 "/new" {:get (authentication-required  cab-handlers/new #{:admin :manager})}
+                 "/delete" {:post (authentication-required  cab-handlers/delete #{:admin :manager})}
+                 ["/" :slug] {"/edit" {:get cab-handlers/update-cab-view}
+                              :get cab-handlers/view-cab
                               :post cab-handlers/update-cab}}]
         ["users" {"/signup" {:get user-handlers/signup-form
                              :post user-handlers/create-user}
                   "/login" {:get user-handlers/login-form
                             :post user-handlers/login}
                   "/logout" {:get user-handlers/logout}
-                  "/dashboard" {:get (authentication-required (wrap-layout dashboard-handlers/index)
+                  "/dashboard" {:get (authentication-required dashboard-handlers/index
                                                               #{:admin :manager :driver})}}]
-        ["fleets" {"" {:get (authentication-required (wrap-layout fleet-handlers/show-fleets) #{:admin})
+        ["fleets" {"" {:get (authentication-required fleet-handlers/show-fleets #{:admin})
                        :post fleet-handlers/create-fleet}
-                   "/new" {:get (wrap-layout fleet-handlers/new)}}]
+                   "/new" {:get fleet-handlers/new}}]
         ["organisations" {"/new" {:post (authentication-required org-handlers/create
                                                                  #{:admin})}}]
-        ["fleets" {"" {:get (authentication-required (wrap-layout fleet-handlers/show-fleets) #{:admin})
+        ["fleets" {"" {:get (authentication-required fleet-handlers/show-fleets #{:admin})
                        :post (authentication-required fleet-handlers/create-fleet #{:admin})}
-                   "/new" {:get (authentication-required (wrap-layout fleet-handlers/new) #{:admin})}}]
+                   "/new" {:get (authentication-required fleet-handlers/new #{:admin})}}]
         ["healthcheck" {:get (wrap-json-response handler/health-check)}]
-        ["index" {:get (wrap-layout handler/index)}]
-        ["" {:get (wrap-layout handler/root)}]
-        [true (wrap-layout handler/not-found)]]])
+        ["index" {:get handler/index}]
+        ["" {:get handler/root}]
+        [true handler/not-found]]])
