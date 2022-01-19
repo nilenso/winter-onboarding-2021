b0e9e2b6e8daf184d85ab2c17e523af24ff45451
Shivam <shivam@nilenso.com>
2022-01-19 21:55:05 +0530

    Use desturcturing to make code more readbable

--- src/winter_onboarding_2021/fleet_management_service/handlers/organisation.clj
+++ src/winter_onboarding_2021/fleet_management_service/handlers/organisation.clj
@@ -3,14 +3,14 @@
             [winter-onboarding-2021.fleet-management-service.models.organisation :as org-models]
             [winter-onboarding-2021.fleet-management-service.utils :as utils]))
 
-(defn create [request]
-  (let [org-name (get-in request [:form-params :name])
-        user (:user request)]
+(defn create [{:keys [user form-params]}]
+  (let [org-name (:name form-params)
+        {:users/keys [org-id role]} user]
     (cond
-      (:users/org-id user) (merge (utils/flash-msg "You are already associated with an organsation", false)
-                                  (response/redirect "/users/dashboard"))
-      (not= "admin" (:users/role user)) (merge (utils/flash-msg "You need admin privileges to create an org", false)
-                                               (response/redirect "/users/dashboard"))
+      org-id (merge (utils/flash-msg "You are already associated with an organsation", false)
+                    (response/redirect "/users/dashboard"))
+      (not= "admin" role) (merge (utils/flash-msg "You need admin privileges to create an org", false)
+                                 (response/redirect "/users/dashboard"))
       :else (do (org-models/create-and-associate {:organisations/name org-name}
                                                  user)
                 (merge (utils/flash-msg "Organsiation created successfully", true)
