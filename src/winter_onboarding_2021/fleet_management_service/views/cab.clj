c9f291e10938d7bdebabf7fa9168305b276ab81f
Alisha Mohanty <alishamohanty1416@gmail.com>
2022-01-19 14:49:39 +0530

    Add a `add a cab` button in cabs list page Rename the heading to add a cab

--- src/winter_onboarding_2021/fleet_management_service/views/cab.clj
+++ src/winter_onboarding_2021/fleet_management_service/views/cab.clj
@@ -4,7 +4,7 @@
 
 (defn cab-form [{:keys [name distance-travelled licence-plate]}]
   [:div {:id "content"}
-   [:h1 {:class "text-success"} "This is the form page"]
+   [:h1 {:class "text-success text-center"} "Add a cab"]
    [:form {:action "/cabs" :method "POST"}
     [:div {:class "mb-3"}
      (core-views/labelled-text-input
@@ -98,6 +98,10 @@
 (defn show-cabs [cabs page-num show-next-page?]
   (let [next-page-query (str "?page=" (inc page-num))]
     [:div
+     [:div {:class "text-end mb-4"}
+      [:a {:id "add-cab" :class "btn btn-primary me-2"
+           :href "/cabs/new"}
+       "Add a cab"]]
      [:table {:class "table table-primary min-vh-40"}
       [:thead [:tr (map (fn [header] [:th (:label header)])
                         headers)]]
