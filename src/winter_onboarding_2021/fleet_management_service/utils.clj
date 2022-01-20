9364767e3d5476da11a54a846195a56d0147960b
Shivam <shivam@nilenso.com>
2022-01-20 11:33:25 +0530

    Rename `hiccup-attrs` to `hiccup-attributes`
    Co-authored-by: Alisha Mohanty <alisha@nilenso.com>

--- src/winter_onboarding_2021/fleet_management_service/utils.clj
+++ src/winter_onboarding_2021/fleet_management_service/utils.clj
@@ -26,6 +26,10 @@
              :style-class "alert alert-danger"
              :message msg}}))
 
+(defn hiccup-attributes [hiccup-element]
+  (when (map? (second hiccup-element))
+    (second hiccup-element)))
+
 (defn- namespace-key [ns k]
   (keyword (str (name ns) "/" (name k))))
 
@@ -46,7 +50,3 @@
 
 (defn rand-str [len]
   (apply str (take len (repeatedly #(char (+ (rand 26) 65))))))
-
-(defn hiccup-attrs [element]
-  (when (map? (second element))
-    (second element)))
