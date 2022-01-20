3ffd454beb507dd2f3401d0fda63abce56582c21
Shivam <shivam@nilenso.com>
2022-01-20 13:22:40 +0530

    Add validations for creating an organisation
    Co-authored-by: Alisha Mohanty <alisha@nilenso.com>

--- src/winter_onboarding_2021/fleet_management_service/models/organisation.clj
+++ src/winter_onboarding_2021/fleet_management_service/models/organisation.clj
@@ -1,11 +1,17 @@
 (ns winter-onboarding-2021.fleet-management-service.models.organisation
-  (:require [winter-onboarding-2021.fleet-management-service.db.organisation :as org-db]
+  (:require [winter-onboarding-2021.fleet-management-service.utils :as utils]
+            [winter-onboarding-2021.fleet-management-service.specs :as specs]
+            [winter-onboarding-2021.fleet-management-service.error :as errors]
+            [winter-onboarding-2021.fleet-management-service.db.organisation :as org-db]
             [winter-onboarding-2021.fleet-management-service.models.user :as user-models]))
 
 (defn create [org]
-  (org-db/create org))
+  (let [org-with-valid-keys (utils/select-keys-from-spec org ::specs/organisations)]
+    (if (empty? org-with-valid-keys)
+      errors/no-valid-keys
+      (org-db/create org-with-valid-keys))))
 
 (defn create-and-associate [org admin]
   (let [db-org (create (assoc org
-                              :created-by (:users/id admin)))]
+                              :organisations/created-by (:users/id admin)))]
     (user-models/add-to-org db-org admin)))
--- test/winter_onboarding_2021/fleet_management_service/models/organisation_test.clj
+++ test/winter_onboarding_2021/fleet_management_service/models/organisation_test.clj
@@ -5,12 +5,16 @@
             [winter-onboarding-2021.fleet-management-service.specs :as specs]
             [winter-onboarding-2021.fleet-management-service.fixtures :as fixtures]
             [winter-onboarding-2021.fleet-management-service.factories :as factories]
+            [winter-onboarding-2021.fleet-management-service.error :as errors]
             [winter-onboarding-2021.fleet-management-service.models.user :as user-models]
             [winter-onboarding-2021.fleet-management-service.models.organisation :as org-models]
             [winter-onboarding-2021.fleet-management-service.db.core :as db-core]))
 
 (use-fixtures :once fixtures/config fixtures/db-connection)
 (use-fixtures :each fixtures/clear-db)
+
+(defn select-relevant-keys [org]
+  (select-keys org [:organisations/name :organisations/created-by]))
 
 (deftest create
   (testing "Should create an organisation in DB"
@@ -20,9 +24,31 @@
                                       (s/gen ::specs/organisations)))
           _ (org-models/create org)
           db-org (first (db-core/query! ["SELECT * FROM organisations;"]))]
-      (is (= org (select-keys db-org
-                              [:organisations/id :organisations/name
-                               :organisations/created-by]))))))
+      (is (= org (select-relevant-keys db-org)))))
+
+  (testing "Should create an organsiation in DB even if supplied data contains extra keys"
+    (let [admin (factories/admin)
+          org  #:organisations{:name "org-1"
+                               :created-by (:users/id admin)
+                               :extra-key "yoo hoo"}]
+      (org-models/create org)
+      (is (= (select-relevant-keys org)
+             (select-relevant-keys (first (db-core/find-by-keys! :organisations
+                                                                 #:organisations{:name "org-1"})))))))
+  
+  (testing "Should not create an organsiation in DB if org name is not present"
+    (let [admin (factories/admin)
+          org  #:organisations{:created-by (:users/id admin)}
+          response (org-models/create org)]
+      (is (= (:error errors/validation-failed) 
+             (:error response)))))
+  
+  (testing "Should not create an organsiation in DB if org name is not present"
+    (let [admin (factories/admin)
+          org  {:name "unqualified-keys"
+                :created-by (:users/id admin)}
+          response (org-models/create org)]
+      (is (= errors/no-valid-keys response)))))
 
 (deftest create-and-associate
   (testing "Should create an organisation and associate the given user with it"
