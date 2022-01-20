33bd2a4f591bababef4042951d40ca21a242d839
Shivam <shivam@nilenso.com>
2022-01-20 13:22:40 +0530

    Add validations for creating an organisation
    Co-authored-by: Alisha Mohanty <alisha@nilenso.com>

--- src/winter_onboarding_2021/fleet_management_service/db/organisation.clj
+++ src/winter_onboarding_2021/fleet_management_service/db/organisation.clj
@@ -1,5 +1,11 @@
 (ns winter-onboarding-2021.fleet-management-service.db.organisation
-  (:require [winter-onboarding-2021.fleet-management-service.db.core :as db]))
+  (:require [clojure.spec.alpha :as s]
+            [winter-onboarding-2021.fleet-management-service.error :as errors]
+            [winter-onboarding-2021.fleet-management-service.specs :as specs]
+            [winter-onboarding-2021.fleet-management-service.db.core :as db]))
 
 (defn create [organisation]
-  (db/insert! :organisations organisation))
+  (if (s/valid? ::specs/organisations organisation)
+    (db/insert! :organisations organisation)
+    (let [error-msg (s/explain-str ::specs/organisations organisation)]
+      (assoc errors/validation-failed :error-msg error-msg))))
