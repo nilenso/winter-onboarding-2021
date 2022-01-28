424c217d174887ed84a541829a809e1840a08e27
Shivam <shivam@nilenso.com>
2022-01-28 13:44:40 +0530

    Don't refer honeysql.helpers directly

--- src/winter_onboarding_2021/fleet_management_service/db/user.clj
+++ src/winter_onboarding_2021/fleet_management_service/db/user.clj
@@ -1,6 +1,6 @@
 (ns winter-onboarding-2021.fleet-management-service.db.user
   (:require [honey.sql :as sql]
-            [honey.sql.helpers :refer [update set where]]
+            [honey.sql.helpers :as h]
             [clojure.spec.alpha :as s]
             [winter-onboarding-2021.fleet-management-service.error :as error]
             [winter-onboarding-2021.fleet-management-service.db.core :as db]
@@ -16,9 +16,9 @@
   (db/find-by-keys! :users key-map))
 
 (defn add-to-org [tx org user]
-  (db/query! tx (sql/format (-> (update :users)
-                             (set {:org-id (:organisations/id org)})
-                             (where [:= :id (:users/id user)])))))
+  (db/query! tx (sql/format (-> (h/update :users)
+                                (h/set {:org-id (:organisations/id org)})
+                                (h/where [:= :id (:users/id user)])))))
 
 (defn members [org role]
   (db/find-by-keys! :users {:role role
