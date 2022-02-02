75b49282679242a3c6a0fea98712ea88566cccaf
Yogiraj H <yogiraj@nilenso.com>
2022-02-02 16:02:25 +0530

    Move recaptcha script from layout to user-view

--- src/winter_onboarding_2021/fleet_management_service/views/user.clj
+++ src/winter_onboarding_2021/fleet_management_service/views/user.clj
@@ -5,41 +5,46 @@
 
 (defn signup-form
   ([] (signup-form nil))
-  ([token] [:form {:action "/users/signup" :method "POST"}
-            [:div {:class "mb-3" :id "email"}
-             (core-views/labelled-text-input
-              "Email address"
-              :required true
-              :id "signup-email-input"
-              :name "email"
-              :type "email")]
-            [:div {:class "mb-3" :id "name"}
-             (core-views/labelled-text-input
-              "Name"
-              :required true
-              :id "signup-name-input"
-              :name "name")]
-            [:div {:class "mb-3" :id "password"}
-             (core-views/labelled-text-input
-              "Password"
-              :required true
-              :id "signup-pwd-input"
-              :type "password"
-              :name "password")]
-            [:input {:type "hidden" :name "role" :value "admin" :id "role"}]
-            (when (some? token)
-              [:input {:type "hidden" :name "token" :value token :id "token"}])
-            [:div {:class "g-recaptcha"
+  ([token] [:div
+            [:script
+             {:defer "defer"
+              :async "async"
+              :src "https://www.google.com/recaptcha/api.js"}]
+            [:form {:action "/users/signup" :method "POST"}
+             [:div {:class "mb-3" :id "email"}
+              (core-views/labelled-text-input
+               "Email address"
+               :required true
+               :id "signup-email-input"
+               :name "email"
+               :type "email")]
+             [:div {:class "mb-3" :id "name"}
+              (core-views/labelled-text-input
+               "Name"
+               :required true
+               :id "signup-name-input"
+               :name "name")]
+             [:div {:class "mb-3" :id "password"}
+              (core-views/labelled-text-input
+               "Password"
+               :required true
+               :id "signup-pwd-input"
+               :type "password"
+               :name "password")]
+             [:input {:type "hidden" :name "role" :value "admin" :id "role"}]
+             (when (some? token)
+               [:input {:type "hidden" :name "token" :value token :id "token"}])
+             [:div {:class "g-recaptcha"
                     :data-sitekey (config/recaptcha-site-key)}]
              [:button {:type "submit" :class "btn btn-primary"} "Submit"]]]))
 
 (defn login-form []
-  [:div [:h1 {:class "text-center"} "Login"] 
+  [:div [:h1 {:class "text-center"} "Login"]
    [:form {:method "POST"}
-   [:div {:class "mb-3" :id "email"}
-    [:label {:for "login-email-input" :class "form-label"} "Email address"]
-    [:input {:required :required :id "login-email-input" :class "form-control" :type "email", :name "email"}]]
-   [:div {:class "mb-3" :id "password"}
-    [:label {:for "login-pwd-input" :class "form-label"} "Password"]
-    [:input  {:required :required :id "login-pwd-input" :class "form-control" :type "password", :name "password"}]]
-   [:button {:type "submit" :class "btn btn-primary"} "Submit"]]])
+    [:div {:class "mb-3" :id "email"}
+     [:label {:for "login-email-input" :class "form-label"} "Email address"]
+     [:input {:required :required :id "login-email-input" :class "form-control" :type "email", :name "email"}]]
+    [:div {:class "mb-3" :id "password"}
+     [:label {:for "login-pwd-input" :class "form-label"} "Password"]
+     [:input  {:required :required :id "login-pwd-input" :class "form-control" :type "password", :name "password"}]]
+    [:button {:type "submit" :class "btn btn-primary"} "Submit"]]])
